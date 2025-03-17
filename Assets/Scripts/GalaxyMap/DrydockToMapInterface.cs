5657d862e801ab227843ab842b939a5ec35fd185
Matt <matthew.acoya@yahoo.com>
2025-03-16 20:34:39 -0500

    V1.5.2.4 - Continuing Ship Prework P2
    Working on drydock to map interface, which handles all ships created, and allows the management of fleets from all ships at the home dock.

--- Assets/Scripts/GalaxyMap/DrydockToMapInterface.cs
+++ Assets/Scripts/GalaxyMap/DrydockToMapInterface.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Script
+/// -Tracks every ship built within the factory
+/// -Allows management of fleets at the factory
+/// </summary>
 public class DrydockToMapInterface : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int FighterCount;
+    public int BomberCount;
+
+    public int GunshipCount;
+    public int TransportCount;
+    public int GunboatCount;
+
+    public int CorvetteCount;
+    public int DestroyerCount;
+    public int FrigateCount;
+
+    public int CruiserCount;
+    public int BattleshipCount;
+    public int CarrierCount;
+
+    public int DreadnoughtCount;
+
+    public List<ShipData> damagedShipsInDock;
+    public List<FleetData> FleetsPresent = new List<FleetData>();
+
+    public TMPro.TMP_InputField fleetNameInput;
+
+    //Opens a simple interface that allows the player to name their new fleet, then opens the manage fleet screen
+    public void CreateNewFleet()
+    {
+        FleetData newFleetData = gameObject.AddComponent<FleetData>();
+        if (fleetNameInput.text != null)
+        {
+            newFleetData.fleetID = fleetNameInput.text;
+            ManageFleet(newFleetData);
+        }
+        else
+        {
+            //Autoname
+            newFleetData.fleetID = "Fleet " + FleetsPresent.Count + 1.ToString() ;
+            ManageFleet(newFleetData);
+        }
+    }
+
+    public void DisbandFleet(FleetData targetFleet)
+    {
+        //Return all ships to drydock
+        //Sort damaged ships into slots
+        //Destroy compoonent
+        Destroy(targetFleet);
+    }
+
+    public void ManageFleet(FleetData targetFleet)
+    {
+        //Get all ships
+        //Display damaged in separate section?
+        // + and - buttons for ships in fleet.
+    }
+
+    public void CloseFleetManager()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void AddFighter(FleetData targetFleet) { if (FighterCount != 0) {  } }
 }

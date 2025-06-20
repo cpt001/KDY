349c509a64c24f7583f904badff9327894d04010
Matt <matthew.acoya@yahoo.com>
2025-06-19 22:58:02 -0500

    V1.5.3.9 - GG3 Continued 10
    -Beginning to explore planet generation
    -Still determining best method and approach.
    
    -> Next
    - Resource generation (Spawns per planet)
    - Interaction UI and planet visual
    - System to system connections
    - Fleet interactions with map
    - Hostile element spawning (radiating fan from capital)
    - Battle system
    - Ultimately, these steps should be tying the system into the factory at large.

--- Assets/Scripts/GalaxyGen3.0/OrbitingBody.cs
+++ Assets/Scripts/GalaxyGen3.0/OrbitingBody.cs
@@ -17,7 +17,6 @@ public class OrbitingBody : MonoBehaviour
         Icy,
         Desert,
         Barren,
-        Rogue,
         ShatteredWorld,
         PlanetaryNebula,
         Derelict,
@@ -33,20 +32,22 @@ public class OrbitingBody : MonoBehaviour
     public float ring2Depth;    //
     public float ring3Depth;    //
     public float atmoDepth;     //Sits above planet as outer layer
+    public Color atmoColor;
     public float cloudDepth;    //Sits slightly above planet
+    public Color cloudColor;
     public float seaDepth;      //Static size of planet, just a simple colored sphere
+    public Color seaColor;
 
     [Header("Body Attributes")]
-    public float bodyTemperature;
+    public int bodyTemperature;
     public float orbitalSpeed;
     public float spinSpeed;
     public bool techtonicallyActive;
     public List<OrbitingBody> moons = new List<OrbitingBody>();
-
-    [Header("Classification Index")]
-    public float Habitability;          //1 - safe (Earth), 2 - mask/covered skin (Mars), 3 - pressurized suit (Moon), 4 - vehicle required (Mustafar), 5 - unsafe (Shattered World)
-    public float EndemicHabitability;   //1 - intelligent life, 2 - sentient, 3 - basic life/trees, 4 - monocellular, 5 - dead
-    public float ColonizationStatus;    //1 - ecumenopolis, 2 - city, 3 - scattered towns, 4 - outposts, 5 - expiditionary, 6 - unexplored
+    public enum AtmosphericHabitability { Safe, Masked, Pressure_Suit, Vehicular, Unsafe }; public AtmosphericHabitability atmosphereHostility;          //Swapped from float, this has better data conveyance
+    public enum EndemicHabitability { Intelligent_Life, Sentient, Basic_Life, Monocellular, Extinct }; public EndemicHabitability endemicHabitation;
+    public enum ColonizationStatus { Ecumenopolis, City, Scattered_Towns, Outposts, Expeditionary, Unexplored }; public ColonizationStatus colonization;    
+    public enum SurvivorStatus { Bunkered_Survivors, Scattered_Survivors, Escapists, LoneSurvivors, TotalDestruction }; public SurvivorStatus survivors;    //No chance any planet is untouched - colonization status determines full range of potential survivors
 
     [Header("Resources Present")]
     public Dictionary<Item, int> resourcesPresent = new Dictionary<Item, int>();
@@ -59,12 +60,277 @@ public class OrbitingBody : MonoBehaviour
         ring2Depth = ring1Depth + 1;
         ring3Depth = ring2Depth + 1;
 
-        atmoDepth = Random.Range(size + 0.5f, size + 2f);
-        cloudDepth = (atmoDepth + size) / 2;
-        seaDepth = size;
-
-
         bodyType = (BodyType)Random.Range(0, System.Enum.GetValues(typeof(BodyType)).Length);
 
+        switch(bodyType)
+        {            
+            case BodyType.Terrestrial:
+                {
+                    //GenPlanet
+                    break;
+                }
+            case BodyType.Volcanic:
+                {
+                    //GenPlanet
+                    break;
+                }
+            case BodyType.Gas:
+                {
+                    //GenPlanet
+                    break;
+                }
+            case BodyType.Oceanic:
+                {
+                    //GenPlanet
+                    break;
+                }
+            case BodyType.Icy:
+                {
+                    //GenPlanet
+                    break;
+                }
+            case BodyType.Desert:
+                {
+                    //GenPlanet
+                    break;
+                }
+            case BodyType.Barren:
+                {
+                    //GenPlanet
+                    break;
+                }
+            case BodyType.ShatteredWorld:
+                {
+                    //SpecialGen
+                    break;
+                }
+            case BodyType.PlanetaryNebula:
+                {
+                    //SpecialGen
+                    break;
+                }
+            case BodyType.Derelict:
+                {
+                    //SpecialGen
+                    break;
+                }
+            case BodyType.BattleSite:
+                {
+                    //SpecialGen
+                    break;
+                }
+            case BodyType.Asteroid:
+                {
+                    //SpecialGen
+                    break;
+                }
+            case BodyType.CometField:
+                {
+                    //SpecialGen
+                    break;
+                }
+        }
+
+    }
+
+    private IEnumerator GeneratePlanetaryBody(BodyType bodyType, bool hasAtmo, bool hasSea)
+    {
+        if (hasAtmo)
+        {
+            atmoDepth = Random.Range(size + 0.5f, size + 2f);
+            cloudDepth = (atmoDepth + size) / 2;
+            atmosphereHostility = (AtmosphericHabitability)Random.Range(0, System.Enum.GetValues(typeof(AtmosphericHabitability)).Length);
+        }
+        if (hasSea)
+        {
+            seaDepth = size;
+        }
+
+        //Determine temperature range based on the habitability || Determine habitability based on the temperature? -- Might be better to determine the body type based on temps instead
+        switch (atmosphereHostility)
+        {            
+            case AtmosphericHabitability.Safe:
+                {
+                    switch (bodyType)
+                    {                        
+                        case BodyType.Terrestrial:
+                            {
+                                bodyTemperature = Random.Range(-40, 150);
+                                break;
+                            }
+                        case BodyType.Volcanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Gas:
+                            {
+                                break;
+                            }
+                        case BodyType.Oceanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Icy:
+                            {
+                                break;
+                            }
+                        case BodyType.Desert:
+                            {
+                                break;
+                            }
+                        case BodyType.Barren:
+                            {
+                                break;
+                            }
+                    }
+                    break;
+                }
+            case AtmosphericHabitability.Masked:
+                {
+                    switch (bodyType)
+                    {
+                        case BodyType.Terrestrial:
+                            {
+                                bodyTemperature = Random.Range(-40, 150);
+                                break;
+                            }
+                        case BodyType.Volcanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Gas:
+                            {
+                                break;
+                            }
+                        case BodyType.Oceanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Icy:
+                            {
+                                break;
+                            }
+                        case BodyType.Desert:
+                            {
+                                break;
+                            }
+                        case BodyType.Barren:
+                            {
+                                break;
+                            }
+                    }
+                    break;
+                }
+            case AtmosphericHabitability.Pressure_Suit:
+                {
+                    switch (bodyType)
+                    {
+                        case BodyType.Terrestrial:
+                            {
+                                bodyTemperature = Random.Range(-100, 200);
+                                break;
+                            }
+                        case BodyType.Volcanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Gas:
+                            {
+                                break;
+                            }
+                        case BodyType.Oceanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Icy:
+                            {
+                                break;
+                            }
+                        case BodyType.Desert:
+                            {
+                                break;
+                            }
+                        case BodyType.Barren:
+                            {
+                                break;
+                            }
+                    }
+                    break;
+                }
+            case AtmosphericHabitability.Vehicular:
+                {
+                    switch (bodyType)
+                    {
+                        case BodyType.Terrestrial:
+                            {
+                                bodyTemperature = Random.Range(-250, 250);
+                                break;
+                            }
+                        case BodyType.Volcanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Gas:
+                            {
+                                break;
+                            }
+                        case BodyType.Oceanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Icy:
+                            {
+                                break;
+                            }
+                        case BodyType.Desert:
+                            {
+                                break;
+                            }
+                        case BodyType.Barren:
+                            {
+                                break;
+                            }
+                    }
+                    break;
+                }
+            case AtmosphericHabitability.Unsafe:
+                {
+                    switch (bodyType)
+                    {
+                        case BodyType.Terrestrial:
+                            {
+                                bodyTemperature = Random.Range(-400, 400);
+                                break;
+                            }
+                        case BodyType.Volcanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Gas:
+                            {
+                                break;
+                            }
+                        case BodyType.Oceanic:
+                            {
+                                break;
+                            }
+                        case BodyType.Icy:
+                            {
+                                break;
+                            }
+                        case BodyType.Desert:
+                            {
+                                break;
+                            }
+                        case BodyType.Barren:
+                            {
+                                break;
+                            }
+                    }
+                    break;
+                }
+        }
+
+        yield return null;
     }
 }
